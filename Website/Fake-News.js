0d13a6e508e94f3809022e6904c825b66ba31243
Amiro Salhi <9014152@student.zadkine.nl>
2026-06-15 13:18:16 +0200

    Navbar mistake

--- Website/Fake-News.js
+++ Website/Fake-News.js
@@ -1,3 +1,12 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
 const questions = [
     {
         question: "Waarom worden deepfakes steeds overtuigender?",
